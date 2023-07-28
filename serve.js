69dad27861b699f9bf472d127ca0a662f1457100
Ev Bogue <ev@evbogue.com>
2023-07-28 10:25:48 -0500

    fish pic

--- serve.js
+++ serve.js
@@ -41,6 +41,8 @@ serve((req, conn) => {
       h('body', null,
         h('img', {src: 'ev.jpg', style: 'width: 33%; float: right;'}),
         h('h1', null, 'Ev\'s Website'),
+        h('p', null, '7/28/23 | I got your message GB, I\'m glad you\'re feeling better. Can you send your location? Here\'s a picture of your fish.'),
+        h('img', {src: './fish.jpg'}),
         h('p', null, '7/27/23 | Hey GB, I got your message and we should coordinate a way to talk soon. Where are you? Send another message in the textarea below, and I will get it.'),
         h('br'),
         h('button', {id: 'about'}, 'About me [New!]'),
